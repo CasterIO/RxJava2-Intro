86512d3f4839e47555c440f6910bdb719c16876d
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-03-02 07:38:43 +0100

    Adds onError and onComplete to the subscription

--- app/src/main/java/io/caster/rxjava2/internals/Subscribers.java
+++ app/src/main/java/io/caster/rxjava2/internals/Subscribers.java
@@ -2,6 +2,7 @@ package io.caster.rxjava2.internals;
 
 
 import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import timber.log.Timber;
 
@@ -12,6 +13,24 @@ public class Subscribers {
             @Override
             public void accept(@NonNull Integer integer) throws Exception {
                 Timber.d("Integer: %d", integer);
+            }
+        };
+    }
+
+    public static Consumer<Throwable> logError() {
+        return new Consumer<Throwable>() {
+            @Override
+            public void accept(@NonNull Throwable throwable) throws Exception {
+                Timber.e(throwable);
+            }
+        };
+    }
+
+    public static Action logOnComplete() {
+        return new Action() {
+            @Override
+            public void run() throws Exception {
+                Timber.d("onComplete() has been called");
             }
         };
     }
