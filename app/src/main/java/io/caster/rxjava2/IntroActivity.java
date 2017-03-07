9b6b94bab09721e7ce442aa6f43bceb413523a4f
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-03-07 07:45:40 +0100

    Reverts the implementation to RxJava 2

--- app/src/main/java/io/caster/rxjava2/IntroActivity.java
+++ app/src/main/java/io/caster/rxjava2/IntroActivity.java
@@ -3,12 +3,12 @@ package io.caster.rxjava2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import rx.Observable;
+import io.reactivex.Observable;
 
-import static io.caster.rxjava2.internals.older.Functions.multiplyNumberBy2;
-import static io.caster.rxjava2.internals.older.Subscribers.logError;
-import static io.caster.rxjava2.internals.older.Subscribers.logInteger;
-import static io.caster.rxjava2.internals.older.Subscribers.logOnComplete;
+import static io.caster.rxjava2.internals.Functions.multiplyNumberBy2;
+import static io.caster.rxjava2.internals.Subscribers.logError;
+import static io.caster.rxjava2.internals.Subscribers.logInteger;
+import static io.caster.rxjava2.internals.Subscribers.logOnComplete;
 
 
 public class IntroActivity extends AppCompatActivity {
